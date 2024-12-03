30910d603c4e9ef88725b3e34cb58f54745f7f89
Xeniaa007 <xenia.jordan@estudiantat.upc.edu>
2024-12-03 17:08:14 +0100

    Update Xènia Jordán Torres - DynamicArray exercise.cpp
    he posat un exercici que no tocava

--- Xènia Jordán Torres - DynamicArray exercise.cpp
+++ Xènia Jordán Torres - DynamicArray exercise.cpp
@@ -12,67 +12,9 @@
 #include <iostream>
 using namespace std;
 
-class array_class {
-private:
-	int mida = 0;
-	int capacitat = 10;
-	int* array;
-
-
-public:
-	array_class() : mida(0), capacitat(0), array(nullptr) { //constructor per defecte
-		array = new int[capacitat];
-	}
-
-	array_class(int m) : mida(m), capacitat(10) {
-		array = new int [capacitat];
-		for (int i = 0; i < mida; i++) {
-			array[i] = i + 1; //afegim valors a l'array, el primer serà 1, perquè la i = 0, i 0+1 = 1, el dos serà 1+1=2, el 3, 2+1 = 3, etc . . .
-		}
-	}
-
-	~array_class() {
-		delete[] array;
-	}
-
-	void reverse_array()
-	{
-		int* array_nova = new int[mida];
-		for (int i = 0; i < mida; i++)
-		{
-			array_nova[i] = array[mida - i - 1]; //comencem per la mida i li anem restant -1 a cada posició, d'aquesta manera la llegim del revés
-		}
-
-		for (int i = 0; i < mida; i++) 
-		{
-			array[i] = array_nova[i]; //tots els valors girats els passem a la array de la classe
-		}
-
-		delete[] array_nova; //com que ja hem fet totes les operacions i no la necessitem més la borrem de la memòria dinàmica
-	}
-
-	void imprimir_array()
-	{
-		for (int i = 0; i < mida; i++)
-		{
-			cout << "[" << array[i] << "]" << endl;
-		}
-	}
-
-};
-
 
 int main()
 {
 
-	array_class array_inventada(10); //creem un objecte de la classe AMB UNA MIDA EN CONCRET
-
-	cout << "Array original: " << endl;
-	array_inventada.imprimir_array();
-
-	cout << "Invertint l'array . . . . " << endl;
-	array_inventada.reverse_array();
-
-	cout << "Resultat final: " << endl;
-	array_inventada.imprimir_array();
+	
 }
