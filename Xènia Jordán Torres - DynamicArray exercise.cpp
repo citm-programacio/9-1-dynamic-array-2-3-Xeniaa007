c4722495f0d695ca1cc7d7469c63cc10e596f895
Xeniaa007 <xenia.jordan@estudiantat.upc.edu>
2024-12-03 17:10:35 +0100

    Update Xènia Jordán Torres - DynamicArray exercise.cpp

--- Xènia Jordán Torres - DynamicArray exercise.cpp
+++ Xènia Jordán Torres - DynamicArray exercise.cpp
@@ -1,14 +1,5 @@
 // Xènia Jordán Torres - DynamicArray exercise.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 
-//Estructura de la solució:
-//1. Crear una classe que contingui una array, la mida de la array i la capacitat del array
-//2. Crear un constructor per inicialitzar totes les variables
-//3. Crear una funció void amb un bucle for que inverteixi l'ordre, haurem de crear una nova array amb new li haurem de dir al bucle que comenci per size-1 (pel final) i llavors aquest nou número ha d'anar a parar a aquesta nova array.
-//4. Caldrà fer un print de la array
-//5. L'haurem de borrar amb delete la vella
-//6. En el main haurem de crear un objecte de la classe
-//7. Cridarem la funció amb l'objecte creat.
-
 #include <iostream>
 using namespace std;
 
